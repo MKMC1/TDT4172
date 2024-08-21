3776c956923eb6146ad06e4707cfd011f4d29adc
Michael <man.k.m.cheung@ntnu.no>
2024-08-21 16:50:51 +0200

    Finished task 1, improvements can be made to runtime

--- Assignment1/linear_regression.py
+++ Assignment1/linear_regression.py
@@ -2,10 +2,11 @@ import numpy as np
 
 class LinearRegression():
     
-    def __init__(self):
+    def __init__(self, learning_rate = 0.001, epochs = 100):
         # NOTE: Feel free to add any hyperparameters 
         # (with defaults) as you see fit
-        pass
+        self.learning_rate = learning_rate
+        self.epochs = epochs
         
     def fit(self, X, y):
         """
@@ -16,9 +17,16 @@ class LinearRegression():
                 m rows (#samples) and n columns (#features)
             y (array<m>): a vector of floats
         """
-        # TODO: Implement
-        raise NotImplementedError("The fit method is not implemented yet.")
-    
+        samples = X.shape[0]
+        self.weight = np.random.randn(len(X.shape))
+        self.bias = 0
+        
+        for epoch in range(self.epochs):
+            grad_feat =  (-2/samples) * X @ (y - (self.weight*X + self.bias))
+            grad_bias = (-2/samples) * np.sum(y - (self.weight*X + self.bias))
+            self.weight -= self.learning_rate * grad_feat
+            self.bias -= self.learning_rate * grad_bias
+            
     def predict(self, X):
         """
         Generates predictions
@@ -32,5 +40,7 @@ class LinearRegression():
         Returns:
             A length m array of floats
         """
-        # TODO: Implement
-        raise NotImplementedError("The predict method is not implemented yet.")
+        return self.weight * X + self.bias
+    
+    def get_params(self):
+        return self.weight, self.bias
